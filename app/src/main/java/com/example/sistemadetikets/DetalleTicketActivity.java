648779f7e59c7b828a8f9a3cc872cc8911cca90d
Piero Dev <pierodev21@gmail.com>
2023-08-25 02:11:11 -0500

    style: Mapeo de Variable de Detalle Ticket

--- app/src/main/java/com/example/sistemadetikets/DetalleTicketActivity.java
+++ app/src/main/java/com/example/sistemadetikets/DetalleTicketActivity.java
@@ -3,12 +3,24 @@ package com.example.sistemadetikets;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DetalleTicketActivity extends AppCompatActivity {
+
+    TextView txtIdTicket,txtNombrePersona,txtTipoTicket,txtDescripcionTicket,txtEstadoTicket,txtSolucionTicket;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalle_ticket);
+
+        txtIdTicket = (TextView) findViewById(R.id.txtIdTicket);
+        txtNombrePersona = (TextView) findViewById(R.id.txtNombrePersona);
+        txtTipoTicket = (TextView) findViewById(R.id.txtTipoTicket);
+        txtDescripcionTicket = (TextView) findViewById(R.id.txtDescripcionTicket);
+        txtEstadoTicket = (TextView) findViewById(R.id.txtEstadoTicket);
+        txtSolucionTicket = (TextView) findViewById(R.id.txtSolucionTicket);
+
+
     }
 }
